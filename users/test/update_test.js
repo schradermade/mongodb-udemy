00c7ea372e85f1b9f591c9b5ca5e86615ca026de
Nathan Schrader <schradermade@Nathans-MBP.netgear.com>
2020-11-29 13:26:03 -0800

    mongodb udemy lesson on UPDATE (crUd)

---
+++ users/test/update_test.js
@@ -0,0 +1,60 @@
+const assert = require('assert');
+const User = require('../src/user');
+
+describe('Updating records', () =>  {
+  let joe;
+
+  beforeEach((done) =>  {
+    joe = new User ({ name: 'Joe' });
+    joe.save()
+      .then(()  =>  done());
+  });
+
+  function assertName(operation, done) {
+    operation
+      .then(()  =>  
+          User.find({}))
+            .then((users)  =>  {
+              assert(users.length === 1);
+              assert(users[0].name === 'Alex');
+              done();
+            });
+  }
+
+  it('instance type using set n save', (done) => {
+    // .set does NOT save change to db, only in memory
+    // use .set to make piece-meal updates
+    joe.set('name', 'Alex');
+    // "persist" record to the database
+    assertName(joe.save(), done);
+  });
+
+  it('A model instance can update', (done) => {
+    // use .update to make a lot of updates all at once
+    assertName(
+      joe.update({ name: 'Alex' }), 
+      done
+      );
+  });
+
+  it('A model class can update', (done) =>  {
+    assertName(
+    User.update({ name: 'Joe' }, { name: 'Alex' }),
+    done
+    );
+  });
+
+  it('A model class can update one record', (done)  =>  {
+    assertName(
+      User.findOneAndUpdate({ name: 'Joe' }, {name: 'Alex'}),
+      done
+    ); 
+  });
+
+  it('A model class can find a record with an Id and update', (done)  =>  {
+    assertName(
+      User.findByIdAndUpdate( joe._id, {name: 'Alex' }),
+      done
+    );
+  })
+});
