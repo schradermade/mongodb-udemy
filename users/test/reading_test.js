4dc7d62abe997d97a952d3aebd9272319998e277
Nathan Schrader <schradermade@Nathans-MBP.netgear.com>
2020-11-29 11:26:26 -0800

    mongodb udemy - 11/29/20

--- users/test/reading_test.js
+++ users/test/reading_test.js
@@ -14,8 +14,18 @@ describe('Reading users out of the database', () => {
     // Here we are referencing User Model/Class, not specific user
     User.find({ name: 'Joe' })
       .then((users)  =>  {
-        console.log(users);
+        console.log(users[0]._id);
+        console.log(joe._id)
+        assert(users[0]._id.toString() === joe._id.toString());
         done();
       });
   });
+
+  it('find a user with a particular id', (done) =>  {
+    User.findOne({ _id: joe._id })
+    .then((user)  =>  {
+      assert(user.name === 'Joe');
+      done();
+    })
+  })
 });
