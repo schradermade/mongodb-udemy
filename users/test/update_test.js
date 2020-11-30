5e897c1a7eb5651daee92fb79986b380186869e6
Nathan Schrader <schradermade@Nathans-MacBook-Pro.local>
2020-11-29 23:49:58 -0800

    finish update_test.js lesson

--- users/test/update_test.js
+++ users/test/update_test.js
@@ -5,7 +5,7 @@ describe('Updating records', () =>  {
   let joe;
 
   beforeEach((done) =>  {
-    joe = new User ({ name: 'Joe' });
+    joe = new User ({ name: 'Joe', postCount: 0 });
     joe.save()
       .then(()  =>  done());
   });
@@ -56,5 +56,13 @@ describe('Updating records', () =>  {
       User.findByIdAndUpdate( joe._id, {name: 'Alex' }),
       done
     );
-  })
+  });
+
+  it('A user can have their postcount incremented by 1', () =>  {
+    User.update({name: 'Joe'}, { $inc: { postCount: 1 } })
+    .then(()  =>  User.findOne({ name: 'Joe' }))
+    .then((user)  =>  {
+      assert(user.postCount === 1);
+    })
+  });
 });
